4a861b5e484d7ca183aae18788e74019e4cbe2b8
STISL <stisl.one@gmail.com>
2021-10-23 12:29:24 -0700

    Update Chap5_ProgEx1.cpp

--- Chap5_ProgEx1.cpp
+++ Chap5_ProgEx1.cpp
@@ -1,4 +1,11 @@
-/* Write a program that prompts the user to input an integer and then outputs both the individual digits of the number and the sum of the digits. For example, it should output the individual digits of 3456 as 3 4 5 6, output the individual digits of 8030 as 8 0 3 0, output the individual digits of 2345526 as 2 3 4 5 5 2 6, output the individual digits of 4000 as 4 0 0 0, and output the individual digits of -2345 as 2 3 4 5. */ 
+/* Write a program that prompts the user to input an integer and then outputs 
+both the individual digits of the number and the sum of the digits. 
+For example, it should output the individual digits of 3456 as 3 4 5 6, 
+output the individual digits of 8030 as 8 0 3 0, 
+output the individual digits of 2345526 as 2 3 4 5 5 2 6, 
+output the individual digits of 4000 as 4 0 0 0, 
+and output the individual digits of -2345 as 2 3 4 5. */
+ 
 #include <iostream>
 #include <string>
 #include <sstream>
