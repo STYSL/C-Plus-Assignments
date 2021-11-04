163ffc5ad9cfb330e63afde4dbc2d2b209ce4eff
STISL <stisl.one@gmail.com>
2021-11-03 20:50:04 -0700

    completed assignment

---
+++ Chap8_ProgEx5.cpp
@@ -0,0 +1,41 @@
+//Malik, D. S.. C++ Programming: From Problem Analysis to Program Design, 8th edition. 
+
+/* Chapter Eight Programming Exercise 5:
+Write a program that prompts the user to input a string 
+and outputs the string in uppercase letters. 
+(Use a character array to store the string.) 
+*/
+
+//AUTHOR: F.RUSTIQUE, JR.
+
+#include <iostream> 
+#include <string>
+//INCLUDE CSTRING LIBRARY FOR C-STRING MANIPULATION:
+#include <cstring>
+
+using namespace std;
+
+//BEGIN MAIN FUNCTION:
+int main(){
+    //DECLARE STRING INPUT VARIABLE:
+    string strInput;
+
+    //DECLARE CHARACTER ARRAY THAT WILL STORE STRING INPUT:
+    char charArray[100] = "";
+    
+    //PROMPT USER TO INPUT A STRING:
+    cout << "Input a string of characters: " << endl;
+    cin.getline(charArray, 100);
+    cout << endl;
+    cout << charArray << " displayed as all uppercase letters is: " << endl;
+
+    //ITERATE THROUGH AND OUTPUT THE C-STRING AS UPPERCASE LETTERS:
+    for (int i = 0; i < strlen(charArray); i++)
+        putchar(toupper(charArray[i])); 
+    //strlen gets length of an array
+    //putchar displays output to console
+    //toupper converts letters to uppercase
+
+    return 0;
+}//END MAIN FUNCTION
+
